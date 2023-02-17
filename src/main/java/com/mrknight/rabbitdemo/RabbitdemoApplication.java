66783bd31f98709e60119b6fdf4f01e6b036e59f
jfcaballerop <jfcaballerop@gmail.com>
2023-02-17 17:09:56 +0100

    first commit

---
+++ src/main/java/com/mrknight/rabbitdemo/RabbitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mrknight.rabbitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrknight/rabbitdemo/RabbitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrknight.rabbitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
